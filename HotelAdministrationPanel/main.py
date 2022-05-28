a0b1ae5637659c3c01d4c5b7e7f593d8cc1d8340
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-05-28 08:17:40 +0300

    Hotel administrator hotel number booking calendar GUI

--- HotelAdministrationPanel/main.py
+++ HotelAdministrationPanel/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from datetime import datetime, timedelta
 
 from HotelDatabase import HotelDatabase
 from HotelServiceRequests import HotelServiceRequests
@@ -8,6 +9,38 @@ from config import db_host, db_port, db_user, db_password, db_name
 
 def btn_command(q):
     print(q)
+    hotel_number_window = Toplevel(root)
+
+    hotel_number_window['bg'] = '#fafafa'
+    hotel_number_window.title('Hotel number ' + str(q) + ' booking')
+    hotel_number_window.geometry('510x284')
+    hotel_number_window.resizable(width=False, height=False)
+
+    current_datetime = datetime.now()
+
+    calendar_x = 1
+    calendar_y = 1
+
+    calendar_buttons_list = []
+    dates_list = []
+
+    for j in range(28):
+
+        dates_list.append(current_datetime + timedelta(days=j))
+
+        calendar_buttons_list.append(
+            Button(hotel_number_window,
+                   text=str(dates_list[j].day) + '/' + str(dates_list[j].month) + '/' + str(dates_list[j].year),
+                   bg='green', width=9, height=4)
+        )
+        calendar_buttons_list[j].grid(column=calendar_x, row=calendar_y)
+
+        calendar_x = calendar_x + 1
+        if calendar_x == 8:
+            calendar_x = 1
+            calendar_y = calendar_y + 1
+
+    print('test')
 
 
 if __name__ == '__main__':
@@ -37,7 +70,7 @@ if __name__ == '__main__':
 
     for i in range(30):
         btn_list.append(
-            Button(frame, text=i, bg='green', width=9, height=4, command=lambda btn_num=i: btn_command(
+            Button(frame, text=i, bg='#fc8403', width=9, height=4, command=lambda btn_num=i: btn_command(
                 btn_num
             ))
         )
